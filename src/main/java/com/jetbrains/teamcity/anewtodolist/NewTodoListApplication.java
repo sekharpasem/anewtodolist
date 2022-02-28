538c8e45e3c1e6ea51892bb127591952d1e5ccc8
Pashem Naveen <pashem.naveen@annalect.com>
2022-02-28 11:52:36 +0530

    Error fixed

--- src/main/java/com/jetbrains/teamcity/anewtodolist/NewTodoListApplication.java
+++ src/main/java/com/jetbrains/teamcity/anewtodolist/NewTodoListApplication.java
@@ -3,7 +3,7 @@ package com.jetbrains.teamcity.anewtodolist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication{
 public class NewTodoListApplication {
 
 	public static void main(String[] args) 
