4da47f8f9b90d019ec103765d4dc40f012f4ccbb
Pashem Naveen <pashem.naveen@annalect.com>
2022-02-28 11:55:24 +0530

    updated

--- src/main/java/com/jetbrains/teamcity/anewtodolist/NewTodoListApplication.java
+++ src/main/java/com/jetbrains/teamcity/anewtodolist/NewTodoListApplication.java
@@ -3,10 +3,10 @@ package com.jetbrains.teamcity.anewtodolist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication{
+@SpringBootApplication
 public class NewTodoListApplication {
 
-	public static void main(String[] args) 
+	public static void main(String[] args){ 
 		SpringApplication.run(NewTodoListApplication.class, args);
 	}
 
