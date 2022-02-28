7ebda52eaf34075c8c07c7737197fdd5a0f8d847
Pashem Naveen <pashem.naveen@annalect.com>
2022-02-28 13:56:35 +0530

    add new data #4

--- src/main/java/com/jetbrains/teamcity/anewtodolist/NewTodoListApplication.java
+++ src/main/java/com/jetbrains/teamcity/anewtodolist/NewTodoListApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NewTodoListApplication {
+public class NewTodoListApplication 
 
 	public static void main(String[] args){ 
 		SpringApplication.run(NewTodoListApplication.class, args);
